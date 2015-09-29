77d7b96695603928e19f9ffb18f22e4dbea96458
plowmanleah <lep55@pitt.edu>
2015-09-28 22:00:33 -0400

    fifth commit

--- pw_check.java
+++ pw_check.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.util.*;
-        
 import java.util.regex.Pattern;
 import java.lang.*;
         
@@ -8,18 +7,22 @@ import java.lang.*;
  *
  * @author plowmanleah
  */
-public class Trie
+public class pw_check
 {
 
     private TrieNode root;
     
-    public Trie() {
+    public pw_check() {
         root = new TrieNode();
     }
  
     public void addWord(String word)
     {
         HashMap<Character, TrieNode> child = root.child;
+        if(word.length() > 5)
+        {
+            return;
+        }
         for(int i = 0; i < word.length(); i++)
         {
             char subChar = word.charAt(i);
@@ -43,13 +46,7 @@ public class Trie
             }
          
         }
-           for(Map.Entry<Character, TrieNode> entry: child.entrySet())
-        {
-            char key = entry.getKey();
-            TrieNode value = entry.getValue();
-            System.out.println(value);
-            System.out.println(key);
-        }
+        
         
     }
     public boolean searchTrie(String word)
@@ -68,20 +65,25 @@ public class Trie
   
     public TrieNode searchNodes(String word)
     {
+        
         Map<Character, TrieNode> child = root.child;
         TrieNode trieNode = null;
         for(int i = 0; i < word.length(); i++)
         {
+            
+            
             char subChar = word.charAt(i);
             if(child.containsKey(subChar))
             {
                 trieNode = child.get(subChar);
                 child = trieNode.child;
+                System.out.println("Password contains unusable word");
                 
             }
             
             else
             {
+                checkForSymbol(word);
                 return null;
             }
         }
@@ -99,40 +101,88 @@ public class Trie
             return true;
         }
     }
-
-
-public class TrieNode {
     
-    public char insertChar;
-    HashMap<Character, TrieNode> child = new HashMap<Character, TrieNode>();
-    boolean isWord;
-    
-    public TrieNode()
+    public boolean testPw(String word)
     {
-        
-    }
-    public TrieNode(char insertChar)
-    {
-        this.insertChar = insertChar;
-    }
-    
-}
-}
-
-public class pw_checkMain {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws FileNotFoundException {
-        Trie tn = new Trie();
-        
-        File file = new File("dictionary.txt");
-        Scanner scan = new Scanner(file);
-        while(scan.hasNext())
+        if(word.length() > 5)
         {
-            String word = scan.next();
-            tn.addWord(word);
+            System.out.println("Password choice is too long.");
+            return false;
         }
+        char character;
+        int countInt = 0;
+        for(int i = 0; i < word.length(); i++)
+        {
+            character = word.charAt(i);
+            if(Character.isLetter(character))
+            {
+                
+                findbegLetter(word);
+            }
+            if(Character.isDigit(character))
+            {
+                countInt++;
+            }
+        }
+        if(countInt == 0 || countInt > 2)
+        {
+            System.out.println("Your password must contain 1-2 numbers");
+        }
+        
+        
+        
+        return false;
+    }
+    
+    public int checkForSymbol(String word)
+    {
+        if(word == null)
+        {
+            return 0;
+        }
+        int counter = 0;
+        char[] symbolsArr = {'!','@','$','%','&','*'};
+        for(int i = 0; i <word.length(); i++)
+        {
+            for(int j = 0; j < symbolsArr.length;j++){
+                char aChar = word.charAt(i);
+                char symbol = symbolsArr[i];
+                if(aChar == symbol)
+                {
+                    counter++;
+                }
+            }
+            
+        }
+        if(counter == 0)
+        {
+            System.out.println("Must contain special characer (!,@,$,%,&,*)");
+        }
+        if(counter >2)
+        {
+            System.out.println("Your password contains too many special characters");
+        }
+        return counter;
+    }
+    
+    private class TrieNode
+    {
+        
+    
+        public char insertChar;
+        HashMap<Character, TrieNode> child = new HashMap<Character, TrieNode>();
+        boolean isWord;
+    
+        public TrieNode()
+        {
+        
+        }
+        public TrieNode(char insertChar)
+        {
+            this.insertChar = insertChar;
+        }
+    
     }
 }
+
+
